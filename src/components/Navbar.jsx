d1889ce1862d6fb82ac2b691593e5414f4f72e72
Cetin CAKIR <cetincakirtr@gmail.com>
2024-02-21 14:52:56 +0300

    feat: First project

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,13 @@
 import scrimbaLogo from '../assets/scrimba-logo.png'
 
 
-function Navbar () {
+function Navbar() {
     return (
-        <div>
-            <img src={scrimbaLogo} className="logo" alt="Scrimba logo" />
-            <h1>Navbar goes here</h1>
-        </div>
+        <nav>
+            <img src={scrimbaLogo} className="nav--icon" />
+            <h3 className="nav--logo_text">ReactFacts</h3>
+            <h4 className="nav--title">React Course - Project 1</h4>
+        </nav>
     )
 }
 
